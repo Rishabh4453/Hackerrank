2cb37740336b19b4008e231215e0524d2c4536f8
Rishabh <rishahdixit1923@gmail.com>
2026-01-31 00:12:01 +0530

    Add day 3 pattern printing programs

---
+++ java/patterns/day3/day_pattern.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class day3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+//        *****
+//        *****
+//        *****
+//        *****
+//        for (int i=1;i<5;i++){
+//            for(int j=1;j<=5;j++){
+//            System.out.print("*");
+//        }
+//            System.out.println("");
+//    }
+
+
+
+
+//        *****
+//        *   *
+//        *   *
+//        *****
+//         int n=4;
+//         int m=5;
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=m;j++) {
+//                 if(i==1 || j==1 || i==n || j==m){
+//                     System.out.print("*");
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+//             }System.out.println("");
+//         }
+
+
+
+
+//        *
+//        **
+//        ***
+//        ****
+//        int n=4;
+//        for(int i=1;i<=n;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+
+
+//        ****
+//        ***
+//        **
+//        *
+//
+//        int n=4;
+//        for(int i=n;i>=1;i--){
+//            for(int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }System.out.println();
+//        }
+
+}}
